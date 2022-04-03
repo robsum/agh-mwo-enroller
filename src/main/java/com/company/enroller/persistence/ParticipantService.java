37814ff3df7e56d86bb391f9586148bc4db9d82f
robsum <sumara.rob@gmail.com>
2022-04-03 15:24:34 +0200

    Getting, adding and deleting added

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,10 +2,13 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 
 import com.company.enroller.model.Participant;
+
+import javax.servlet.http.Part;
 
 @Component("participantService")
 public class ParticipantService {
@@ -22,4 +25,36 @@ public class ParticipantService {
 		return query.list();
 	}
 
+	public Participant findByLogin(String login) {
+		for (Participant participant : getAll()) {
+			if (participant.getLogin().equals(login)) {
+				return participant;
+			}
+		}
+		return null;
+	}
+
+	public void addParticipant(Participant participant) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+
+		session.beginTransaction();
+		session.save(participant);
+		session.getTransaction().commit();
+	}
+
+	public void delete(Participant participant) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+
+		session.beginTransaction();
+		session.delete(participant);
+		session.getTransaction().commit();
+	}
+
+	public void updateParticipant(Participant participant) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+
+		session.beginTransaction();
+		session.merge(participant);
+		session.getTransaction().commit();
+	}
 }
